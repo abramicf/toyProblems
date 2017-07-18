47d4ef4f5aea126e527bee257f392bb3f1d1feaf
Chris Abrami <abramicf@gmail.com>
2017-07-18 10:07:23 -0700

    refactored version of algorithm that passes all tests

--- balancedParens/spec/test.js
+++ balancedParens/spec/test.js
@@ -49,18 +49,48 @@ describe('balancedParens', () => {
     balancedParens(test).should.be.true;
   });
 
-  //second pass tests
-
-
   it('should return false for "([{})]{}"', () => {
     let test = '([{})]{}';
     balancedParens(test).should.be.false;
   });
 
-  // it('should return false for "()[]{"', () => {
-  //   let test = '';
-  //   balancedParens(test).should.be.false;
-  // });
+  it('should return false for "()[]{"', () => {
+    let test = '()[]{';
+    balancedParens(test).should.be.false;
+  });
+
+  it('should return false for "()[({(){[}]})]"', () => {
+    let test = '()[({(){[}]})]';
+    balancedParens(test).should.be.false;
+  });
+
+  it('should work on actual functions (bubbleSort)', () => {
+    let bubbleSort = (arr) => {
+      if (!arr || !Array.isArray(arr)) {
+        return 'please enter in an array of numeric values';
+      }
+
+      let countSwitch;
+      //set a while loop
+      while (countSwitch !== 0) {
+        countSwitch = 0;
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i + 1] < arr[i]) {
+            //do a switch
+            var firstVal = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = firstVal;
+            countSwitch ++;
+          }
+        }
+      }
+      return arr;
+      };
+
+    let bubbleSortString = bubbleSort.toString();
+
+    balancedParens(bubbleSortString).should.be.true;
+  });
 
   // it('should return true for ""', () => {
   //   let test = '';
--- balancedParens/src/javascript.js
+++ balancedParens/src/javascript.js
@@ -1,42 +1,30 @@
-//Third Pass
-
-//Second Pass
 balancedParens = (string) => {
-  //create storage
-  let storage = {
-    '(': {opposite: ')', count: 0},
-    ')': {opposite: '(', count: 0},
-    '{': {opposite: '}', count: 0},
-    '}': {opposite: '{', count: 0},
-    '[': {opposite: ']', count: 0},
-    ']': {opposite: '[', count: 0},
+  let storage = [];
+  let openings = ['[', '{', '('];
+  let closings = [']', '}', ')'];
+  let opposites = {
+    ')': '(',
+    ']': '[',
+    '}': '{'
   };
-  let total = 0;
 
   for (let char of string) {
-    if (storage[char]) {
-      total += 1;
-      if (char === ')' || char === '}' || char === ']') {
-        let closingChar = storage[char];
-        let openingChar = storage[storage[char]['opposite']];
-        if (openingChar.count === 0 || closingChar.count === openingChar.count) {
-          return false;
-        }
-//something should only be able to 'close' if all other parens are balanced (i.e. if their counts are equal).  So, prior to closing ANYTHING off, we should check the counts for the other two types to make sure they're equal.
-        if (char === ')') {
-          let testOne = storage[']']['count'] < storage['[']['count'];
-          let testTwo = storage['}']['count'] < storage['{']['count'];
-          if (testOne || testTwo) {
-            return false;
-          }
-        }
+    //if opening
+    if (openings.includes(char)) {
+      storage.push(char);
+    } else if (closings.includes(char)) {
+      let opposite = opposites[char];
+      let lastIndex = storage[storage.length - 1];
+      if (opposite !== lastIndex) {
+        return false;
+      } else {
+        let popped = storage.pop();
       }
+    } else {
+      continue;
     }
-    storage[char].count += 1;
   }
-
-
-  if (total % 2 !== 0) {
+  if (storage.length > 0) {
     return false;
   }
   return true;
