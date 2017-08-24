7d89ab83fa8774d3eb3a2e4302dfc7167960c37e
Chris Abrami <abramicf@gmail.com>
2017-08-24 09:41:55 -0700

    have working basic graph traversal

--- depthFirstSearch/spec/test.js
+++ depthFirstSearch/spec/test.js
@@ -23,20 +23,32 @@ describe('Graph Tests', () => {
     should.exist(Graph);
   });
   it('addVertex method should exist and work', () => {
-    let myGraph = new Graph();
-    should.exist(myGraph.addVertex);
-    myGraph.addVertex(3);
-    myGraph.addVertex(2);
-    myGraph.addEdge(2, 3);
-    myGraph.printGraph();
-    myGraph.removeEdge(3, 2);
-    myGraph.printGraph();
-    myGraph.removeEdge(5, 3);
-    myGraph.removeEdge(3, 2);
+    // let myGraph = new Graph();
+    // should.exist(myGraph.addVertex);
+    // myGraph.addVertex(3);
+    // myGraph.addVertex(2);
+    // myGraph.addEdge(2, 3);
+    // myGraph.printGraph();
+    // myGraph.removeEdge(3, 2);
+    // myGraph.printGraph();
+    // myGraph.removeEdge(5, 3);
+    // myGraph.removeEdge(3, 2);
 
-    let myGraphNums = new Graph();
-    should.exist(myGraphNums)
+    let myGraphNames = new Graph();
+    should.exist(myGraphNames);
+    myGraphNames.addVertex('Chris');
+    myGraphNames.addVertex('Uma');
+    myGraphNames.addVertex('Rudy');
+    myGraphNames.addVertex('Greg');
+    myGraphNames.addEdge('Chris', 'Rudy');
+    myGraphNames.addEdge('Uma', 'Pete');
+    myGraphNames.addEdge('Chris', 'Uma');
+    myGraphNames.addEdge('Uma', 'Rudy');
+    myGraphNames.addEdge('Chris', 'Greg');
+    myGraphNames.addEdge('Uma', 'Greg');
+    myGraphNames.printGraph();
 
+    depthFirstSearchGraph(myGraphNames, 'Chris');
 
   });
   // it('Graph class should exist', () => {
--- depthFirstSearch/src/javascript.js
+++ depthFirstSearch/src/javascript.js
@@ -16,28 +16,20 @@ class BinaryTreeNode {
 class Graph {
   constructor() {
     this.storage = {};
-    this.numberOfEdges = 0;
   }
 
   addVertex(vertex) {
     if (!this.storage[vertex]) {
-      this.storage[vertex] = []; //create empty edges array
+      this.storage[vertex] = [];
     }
-    //if it already exists, don't do anything!
-  }
-
-  removeVertex(vertex) {
-    //first, check if vertex exists in the graph
   }
 
   addEdge(vertex1, vertex2) {
-    //Assumes we'll add the second vetex to the graph if it's not there already
-    //Check whether both are there and create them if they're not
     if (!this.storage[vertex1]) {
-      addVertex(vertex1);
+      this.addVertex(vertex1);
     }
     if (!this.storage[vertex2]) {
-      addVertex(vertex2);
+      this.addVertex(vertex2);
     }
     //add vertex2 to the array for vertex1
     this.storage[vertex1].push(vertex2);
@@ -45,47 +37,71 @@ class Graph {
     this.storage[vertex2].push(vertex1);
   }
 
-  removeEdge(vertex1, vertex2) {
-    //if one or both verticies aren't in the graph
-    if (!this.storage[vertex1] || !this.storage[vertex2]) {
-      let message = 'verticies not in this graph';
-      console.log(message);
-      return message;
-    }
-
-    //if the edge doesn't exist
-    if (!this.storage[vertex1].includes(vertex2) || !this.storage[vertex2].includes(vertex1)) {
-      let message = 'edge does not exist';
-      console.log(message);
-      return message;
-    }
-
-    let edgeRemoved = [];
-    // console.log(this.storage);
-    // console.log(this.storage[vertex2]);
-    let firstIndex = this.storage[vertex1].indexOf(vertex2);
-    let firstRemoved = this.storage[vertex1].splice(firstIndex, 1);
-    // console.log(firstRemoved);
-    edgeRemoved.push(firstRemoved);
-    let secondIndex = this.storage[vertex2].indexOf(vertex1);
-    let secondRemoved = this.storage[vertex2].splice(secondIndex, 1);
-    // console.log(secondRemoved);
-    edgeRemoved.push(secondRemoved);
-    // console.log(edgeRemoved);
-    //if the edge does exist
-  }
-
-  printGraph() {
+   printGraph() {
     for (let prop in this.storage) {
       console.log(prop + ' : ' + this.storage[prop]);
     }
   }
+
+    // removeVertex(vertex) {
+  //   //first, check if vertex exists in the graph
+  // }
+
+  // removeEdge(vertex1, vertex2) {
+  //   //if one or both verticies aren't in the graph
+  //   if (!this.storage[vertex1] || !this.storage[vertex2]) {
+  //     let message = 'verticies not in this graph';
+  //     console.log(message);
+  //     return message;
+  //   }
+
+  //   //if the edge doesn't exist
+  //   if (!this.storage[vertex1].includes(vertex2) || !this.storage[vertex2].includes(vertex1)) {
+  //     let message = 'edge does not exist';
+  //     console.log(message);
+  //     return message;
+  //   }
+
+  //   let edgeRemoved = [];
+  //   // console.log(this.storage);
+  //   // console.log(this.storage[vertex2]);
+  //   let firstIndex = this.storage[vertex1].indexOf(vertex2);
+  //   let firstRemoved = this.storage[vertex1].splice(firstIndex, 1);
+  //   // console.log(firstRemoved);
+  //   edgeRemoved.push(firstRemoved);
+  //   let secondIndex = this.storage[vertex2].indexOf(vertex1);
+  //   let secondRemoved = this.storage[vertex2].splice(secondIndex, 1);
+  //   // console.log(secondRemoved);
+  //   edgeRemoved.push(secondRemoved);
+  //   // console.log(edgeRemoved);
+  //   //if the edge does exist
+  // }
+
 }
 
 //traverse a graph!
 
 //you'd have to have an entry point
-//you'd have to
+//just have a vertext you want to start at!
+let depthFirstSearchGraph = (graph, start) => {
+  let result = [];
+
+  let subroutine = (vertex) => {
+    if (result.includes(vertex)) {
+      return;
+    } else {
+      let connections = graph['storage'][vertex];
+      // console.log(connections);
+      result.push(vertex);
+      for (let i = 0; i < connections.length; i++) {
+        subroutine(connections[i]);
+      }
+    }
+  };
+  subroutine(start);
+  console.log(result);
+  return result;
+};
 
 //traverse a tree
 
